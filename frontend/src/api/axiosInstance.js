9022a8ad25a3b5b71b40e3198a3f5016fa3f4b86
Himanshu Singh Bhadoriya <149147778+bhadoriya7898@users.noreply.github.com>
2025-12-06 19:45:26 +0530

    live

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://repairwala.vercel.app/",
+  baseURL: "https://repairwalaunitech.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
